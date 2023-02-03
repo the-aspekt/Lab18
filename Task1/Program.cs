37e8c81967886cd466a46d81a20619df83fa8eff
Pavel <sozonovps@gmail.com>
2023-02-03 20:38:49 +0800

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine(Check(str));
+            Console.ReadLine();
+
+        }
+
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> sk = new Dictionary<char, char>
+            {
+                { '(',')' },
+                { '[',']' },
+                {'{','}' },
+                { '<','>' },
+
+            };
+        foreach (char c in str)
+            {
+               if (c == '(' || c == '{' || c == '[' || c == '<')
+                    stack.Push(sk[c]);
+               else if (c == '}' || c == ']' || c == ')' || c == '>')
+                {
+                    if (stack.Count == 0 || stack.Pop()!=c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
